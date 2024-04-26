30d2c70287041d63c701d49b47b67f471f9f39a7
Your Name <you@example.com>
2024-04-26 22:34:22 +0530

    Section Divider  Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
-  return <main className="flex flex-col items-center px-4">
-    <Intro />
-    
-  </main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+    </main>
+  );
 }
