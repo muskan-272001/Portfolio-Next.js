2ccee05256f272d9b621450a58c198687e91dd17
Your Name <you@example.com>
2024-04-29 20:46:58 +0530

    Skills and Experience Section Done

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-import {motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -15,27 +15,29 @@ const fadeInAnimationVariants = {
   animate: (index: number) => ({
     opacity: 1,
     y: 0,
-    transition : {
-        delay: 0.05 * index,
+    transition: {
+      delay: 0.05 * index,
     },
   }),
 };
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
+
   return (
     <section
+      id="skills"
       ref={ref}
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
-      <SectionHeading>My Skills</SectionHeading>
+      <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
             className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
             key={index}
             variants={fadeInAnimationVariants}
-            initial="intial"
+            initial="initial"
             whileInView="animate"
             viewport={{
               once: true,
