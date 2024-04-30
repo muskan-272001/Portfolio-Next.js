b88ebf9b6ce8417d4a3ba8ce9100baf829726967
Your Name <you@example.com>
2024-04-30 22:07:08 +0530

    Dark Mode Added

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,14 +11,12 @@ import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  const {ref} = useSectionInView("Home", 0.5);
-  const {
-    setActiveSection,
-    setTimeOfLastClick
-  } = useActiveSectionContext();
-  
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-    <section ref={ref}
+    <section
+      ref={ref}
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       id="home"
     >
@@ -81,7 +79,8 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+          outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -91,10 +90,10 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        
-
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border borderBlack"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+          focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border borderBlack 
+          dark:bg-white/10"
           href="/CV.pdf"
           download
         >
@@ -103,7 +102,9 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full 
+          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition 
+          cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/muskanpandey2711/"
           target="_blank"
         >
@@ -111,7 +112,9 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] 
+          rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 
+          transition cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/muskan-272001"
           target="_blank"
         >
